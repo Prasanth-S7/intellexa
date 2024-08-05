a0aae4d5cd1b6c73b9889db447e4bb04d1476e60
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-05 16:58:22 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { HeroSectionClient } from '@/components/heroSection';
 import { TeamSpotlight } from '@/components/teamSpotlight';
+import { Footer } from '@/components/footer';
 
 export default function Home() {
   return (
       <div>
         <HeroSectionClient />
         <TeamSpotlight />
+        <Footer />
       </div>
   );
 }
