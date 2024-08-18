f76dda3def24389cbd439ce05aa7018c0211aacb
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-18 12:40:17 +0530

    reponsiveness in herosection

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -12,7 +12,7 @@ const Menu = ({navItems}) => {
     <div className="menu-container relative" ref={container}>
       <div className="menu-bar flex justify-end items-center p-4 bg-transparent text-white">
         <div className="menu-open cursor-pointer" onClick={toggleMenu}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
         </div>
       </div>
 
--- src/components/sparkles-title.jsx
+++ src/components/sparkles-title.jsx
@@ -5,7 +5,7 @@ import { FlipWordsDemo } from "./innovate-inspire";
 import { HeroHighlightDemo } from "./heroHighlight";
 export function SparklesTitle() {
     return (
-        <div className=" bg-transparent overflow-x-hidden w-full bg-black flex flex-col items-center justify-end overflow-hidden rounded-md">
+        <div className=" bg-transparent overflow-x-hidden w-full h-[650px] bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
             <h1 className="md:text-7xl text-[#9207fb] text-5xl lg:text-7xl font-bold text-center relative z-20 mt-0">
                 <div>
                     <div className="text-white mb-10 font-slussenLight text-2xl font-semibold"><HeroHighlightDemo></HeroHighlightDemo></div>
