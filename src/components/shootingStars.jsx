9af9a579e283d2a4a5eac693ee5960b621a829df
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-18 12:20:16 +0530

    reponsiveness in herosection

--- src/components/shootingStars.jsx
+++ src/components/shootingStars.jsx
@@ -7,7 +7,7 @@ import { FlipWordsDemo } from "./innovate-inspire";
 import { SparklesTitle } from "./sparkles-title";
 export function ShootingStarsAndStarsBackgroundDemo({ children }) {
     return (
-        <div className="min-h-screen rounded-md bg-black flex flex-col items-center justify-center relative w-full  py-0">
+        <div className="min-h-screen rounded-md bg-black flex flex-col items-center justify-start md:justify-center relative w-full  py-0">
             <h2 className="relative flex-col md:flex-row z-10 text-4xl md:text-7xl md:leading-tight max-w-5xl mx-auto text-center font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
                 {/* <div>
                     <div className="text-white mb-10 font-slussenLight text-2xl font-semibold">The place where creativity meets invention: striving for groundbreaking achievements one step at a time.</div>
