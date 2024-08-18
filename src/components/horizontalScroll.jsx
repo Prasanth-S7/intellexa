10c56cf07ab9e086e1673e1dc9d29a349be0f7f9
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-18 11:02:23 +0530

    added responsiveness for why join intellexa

--- src/components/horizontalScroll.jsx
+++ src/components/horizontalScroll.jsx
@@ -2,7 +2,7 @@ export const HorizontalScroll = (({ number, info, className }) => {
     return (
         <div className={`w-full h-[400px] md:h-[400px] md:w-[400px] border-2 border-white px-7 py-7 flex flex-col justify-between ${className}`} >
             <div className="text-custom-yellow lg:text-5xl md:text-5xl text-4xl font-slussen ">{number}</div>
-            <div className="text-white text-[20px] font-semibold font-slussenLight ">{info}</div>
+            <div className="text-white text-[20px] font-slussenLight ">{info}</div>
         </div>
     )
 })
