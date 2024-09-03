eeaaed07d65737797b0d315a77c392fd40d8e863
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-09-03 18:55:14 +0530

    final

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
+'use client';
+import dynamic from 'next/dynamic';
 import { HeroSectionClient } from '@/components/heroSection';
 import { TeamSpotlight } from '@/components/whyjoinIntellexa';
-import { Footer } from '@/components/footer';
-// import { FeatureCards } from '@/components/feature-cards';
-
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <div>
+      <AnimatedCursor
+        innerSize={20}
+        outerSize={15}
+        color='146, 8, 255'
+        outerAlpha={0.2}
+        innerScale={0.7}
+        hasBlendMode={true}
+        outerScale={5}
+        outerStyle={{
+          mixBlendMode: 'exclusion'
+        }} />
       <HeroSectionClient />
       <section className='overflow-hidden'>
         <TeamSpotlight />
