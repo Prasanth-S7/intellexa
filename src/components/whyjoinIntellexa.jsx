ede9ef526554bf652d417c69f5532494fe68367b
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-10-06 18:53:09 +0530

    check

--- src/components/whyjoinIntellexa.jsx
+++ src/components/whyjoinIntellexa.jsx
@@ -152,7 +152,7 @@ export const TeamSpotlight = () => {
         <div className="flex flex-col items-start">
           <div className="space-x-14 text-6xl flex-col justify-start mb-[8vh]">
             <div className="px-[5px] lg:text-[8vh] md:text-5xl text-4xl text-white font-slussen font-bold border-l-4 pl-5 border-custom-purple leading-tight lg:leading-none">
-              WHY JOIN<br />
+              WHY JOINsss<br />
               <span>INTELLEXA?</span>
             </div>
           </div>
