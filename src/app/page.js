2ea432d37104f1c21f9a905bdaa3bf61f35be6a6
jeffron025 <jaffyjeffrin@gmail.com>
2024-10-22 00:01:37 +0530

    bg update

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,19 @@
 import dynamic from 'next/dynamic';
 import { HeroSectionClient } from '@/components/heroSection';
 import { TeamSpotlight } from '@/components/whyjoinIntellexa';
+import {StarsBackground} from "../components/ui/stars-background"
 const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
   ssr: false,
 });
 export default function Home() {
   return (
-    
     <div className='overflow-x-hidden'>
       <HeroSectionClient />
+      <StarsBackground/>
       <section className='overflow-hidden'>
         <TeamSpotlight />
       </section>
     </div>
+   
   );
 }
